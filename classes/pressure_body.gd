8b8b4907902d737e1b6d92b49677dba7a75e5e58
Alex <119821293+Devallex@users.noreply.github.com>
2025-06-13 07:52:41 -0400

    Calculate scalar 2

--- classes/pressure_body.gd
+++ classes/pressure_body.gd
@@ -12,9 +12,19 @@ func _affect_size(size: float):
 	scale = Vector3.ONE * size
 
 func _calculate_scalar() -> float:
-	var pressure_difference: float = pressure / max(0.001, atmosphere.pressure)
-	var scalar = 1e-4 * scalar_weight * (pressure_difference - 1.0) + 0.2
-	return scalar
+	var pressure_difference: float = pressure / max(0.001, atmosphere.pressure)**2
+	#var scalar = 1e-3 * scalar_weight * (pressure_difference - 1.0) + 0.2
+	#return scalar
+	var max_pressure = 20.0
+	var min_scalar = 0.05
+	var max_scalar = 3.0
+	var time_to_max_scalar: float = 10000.0
+	var clamped_pressure = clamp(pressure_difference, 0.0, time_to_max_scalar)
+	var normalized = clamped_pressure / time_to_max_scalar
+	var curved = pow(normalized, 1.0)
+	return lerp(min_scalar, max_scalar, curved)
+	return lerp(min_scalar, max_scalar, curved)
+
 
 func _process(delta: float) -> void:
 	super(delta)
